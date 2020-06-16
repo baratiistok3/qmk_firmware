851614038d929cbba321915edd09a2283e07aed5
git <baratiistok3@gmail.com>
2020-06-16 19:24:54 +0200

    ctrl

--- keyboards/40percentclub/gherkin/keymaps/sajat_gherkin/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/sajat_gherkin/keymap.c
@@ -14,7 +14,8 @@ enum keyboard_layers {
   _QWERTY,
   _ADJUST,
   _MARKS,
-  _NUMS
+  _NUMS,
+  _CTRL
 };
 
 enum combos {
@@ -32,6 +33,7 @@ enum combos {
 #define ADJ_Z   LT(_ADJUST, KC_Z)
 #define MARKS_A   LT(_MARKS, KC_A)
 #define NUMS_S   LT(_NUMS, KC_S)
+#define CTRL_ENT LT(_CTRL, KC_ENT)
 
 const uint16_t PROGMEM sdf_combo[] = {KC_S, KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM jkl_combo[] = {KC_J, KC_K, KC_L, COMBO_END};
@@ -71,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_ortho_3x10(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,   TD(TD_O_OO),    KC_P,
     MARKS_A,    NUMS_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_RSFT,
-    ADJ_Z,   TD(TD_X_WIN),    KC_C,    KC_V,    KC_BSPC, KC_SPC,  KC_B,    KC_N,    KC_M,    KC_ENT
+    ADJ_Z,   TD(TD_X_WIN),    KC_C,    KC_V,    KC_BSPC, KC_SPC,  KC_B,    KC_N,    KC_M,    CTRL_ENT
   ),
 
 
@@ -91,6 +93,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_7, KC_8, KC_9,  XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_GRV, KC_1, KC_2, KC_3, XXXXXXX
+  ),
+    [_CTRL] = LAYOUT_ortho_3x10(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,
+     XXXXXXX, KC_A, KC_C, KC_V, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_LCTL, XXXXXXX, KC_X, KC_Y, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
 };
 
